14fd9fef8d4f4ece0295854f023296ab09678cfe
siromanto <dmytrenko.serhii@gmail.com>
2017-07-18 00:35:56 +0300

    simulate back-end work in promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,7 +82,7 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
